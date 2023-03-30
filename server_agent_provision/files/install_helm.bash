94d5df73965d6723efb9b8db647b4df967aed82a
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-03-31 00:32:57 +0200

    Need helm version3
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- server_agent_provision/files/install_helm.bash
+++ server_agent_provision/files/install_helm.bash
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
 
-export PATH=${PATH}:/usr/local/bin
-
 # Download & install Helm
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
-chmod u+x install-helm.sh
-./install-helm.sh
-rm install-helm.sh
-
-# Need to install tiller service to handle heml locally
-kubectl -n kube-system create serviceaccount tiller
-
-kubectl create clusterrolebinding tiller \
-  --clusterrole=cluster-admin \
-  --serviceaccount=kube-system:tiller
-
-helm init --service-account tiller
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm get_helm.sh
