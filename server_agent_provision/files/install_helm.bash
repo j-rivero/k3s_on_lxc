2843e4d4493d58616bc1568b2961c5db0d542dfa
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-03-30 23:09:31 +0200

    Install helm in server
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

---
+++ server_agent_provision/files/install_helm.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download & install Helm
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
+chmod u+x install-helm.sh
+./install-helm.sh
+rm install-helm.sh
