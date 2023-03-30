e8b4a50111f2df78312be3edaae77047aa0bea11
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-03-30 23:44:54 +0200

    Install Tiller
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- server_agent_provision/files/install_helm.bash
+++ server_agent_provision/files/install_helm.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+
+export PATH=${PATH}:/usr/local/bin
 
 # Download & install Helm
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
@@ -6,4 +9,11 @@ chmod u+x install-helm.sh
 ./install-helm.sh
 rm install-helm.sh
 
-/usr/local/bin/helm init
+# Need to install tiller service to handle heml locally
+kubectl -n kube-system create serviceaccount tiller
+
+kubectl create clusterrolebinding tiller \
+  --clusterrole=cluster-admin \
+  --serviceaccount=kube-system:tiller
+
+helm init --service-account tiller
