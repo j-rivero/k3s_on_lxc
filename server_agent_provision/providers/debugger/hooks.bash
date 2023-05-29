df2e91d07bcb0847a73e2b721c9884f0bb1b9ca9
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-05-29 18:30:28 +0200

    Implement a dummy provider showing the bash commands executed
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

---
+++ server_agent_provision/providers/debugger/hooks.bash
@@ -0,0 +1,40 @@
+#
+# Promox provider
+#
+# Use pct commmands from the system host to execute installation
+# on Proxmox LXCs.
+#
+
+#
+# Implement the exec and exec_file hook
+# For Proxmox system base use _pvt_ functqions
+#
+
+hook_exec() {
+  local SYSTEM_FILE=${1} CMD=${2} ENABLE_OUTPUT=${3:-false} ERR=false
+
+  echo "sh -c \"${CMD}\"" >> "${SYSTEM_FILE}"
+
+  [[ ${ENABLE_OUTPUT} ]] && echo "${LOG}"
+  return 0
+}
+
+hook_exec_file() {
+  local SYSTEM_FILE=${1} FILE_TO_EXEC=${2} ARG1=${3} ARG2=${4} ARG3=${5} ERR=false
+
+  echo "# Running file: ${FILES_DIR}/${FILE_TO_EXEC}" >> ${SYSTEM_FILE}
+  echo "${FILES_DIR}/${FILE_TO_EXEC} \"${ARG1}\" \"${ARG2}\" \"${ARG3}\"" >> ${SYSTEM_FILE}
+  return 0
+}
+
+#
+# Implement the hook_provision_platform:
+# Use plain text files for debugging
+#
+hook_provision_platform() {
+  VMID_SERVER="/tmp/.k3_debugger_vmid_server"
+  VMID_AGENT="/tmp/.k3_debugger_vmid_agent"
+
+  echo "[ --- ] Created debug system server in file ${VMID_SERVER}"
+  echo "[ --- ] Created debug system agent in file ${VMID_AGENT}"
+}
