35e8b89bebdbbe1b64d8e59060251960152f5691
Jose Luis Rivero <jrivero@osrfoundation.org>
2023-09-11 20:27:49 +0200

    Support for SERVER_ONLY option
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- server_agent_provision/create_k3s_server_agent_proxmox.bash
+++ server_agent_provision/create_k3s_server_agent_proxmox.bash
@@ -14,6 +14,7 @@ PROVIDER_HOOK_FILE="${PROVIDER_DIR}/hooks.bash"
 # -------------------------------------------
 # Useful variables
 #
+ONLY_SERVER=${ONLY_SERVER:-false}
 DEBUG=${DEBUG:-false}
 # -------------------------------------------
 
@@ -59,13 +60,15 @@ fi
 SERVER_TOKEN=$(hook_exec ${VMID_SERVER} "cat /var/lib/rancher/k3s/server/node-token" true)
 SERVER_IP=$(hook_exec ${VMID_SERVER} "ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'" true)
 
-# Agent installation
-echo "[ --- ]"
-echo "[ AGENT ] Install the k3s agent"
-hook_exec_file ${VMID_AGENT} "install_k3s_agent.bash" "${SERVER_IP}" "${SERVER_TOKEN}"
-echo -n "[ TEST ] Check server connection"
-hook_exec ${VMID_SERVER} "/usr/local/bin/kubectl get nodes | grep -q ${CLUSTER_INSTANCES[VMID_AGENT]}"
-echo "[ --- ]"
+if ! ${ONLY_SERVER}; then
+  # Agent installation
+  echo "[ --- ]"
+  echo "[ AGENT ] Install the k3s agent"
+  hook_exec_file ${VMID_AGENT} "install_k3s_agent.bash" "${SERVER_IP}" "${SERVER_TOKEN}"
+  echo -n "[ TEST ] Check server connection"
+  hook_exec ${VMID_SERVER} "/usr/local/bin/kubectl get nodes | grep -q ${CLUSTER_INSTANCES[VMID_AGENT]}"
+  echo "[ --- ]"
+fi
 
 # Install the helm packages in the server from the configuration file
 _install_helm_packages "${VMID_SERVER}"
